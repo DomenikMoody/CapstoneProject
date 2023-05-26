8d0425ed48a0f1cc6e8a06c45eb5c4952c7e4051
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-26 17:56:33 -0400

    fixed models

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -10,7 +10,7 @@ class Playlist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
-    name = db.Column(db.String(40), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
     playlist_image = db.Column(db.String(255), nullable=False)
 
     playlist_videos = db.relationship(
